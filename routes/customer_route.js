b29b1da8609233cca39533a78460fe364d49f36a
Suyogya Shrestha <suyogyashrestha@Suyogyas-MacBook-Air.local>
2025-01-08 08:03:34 +0545

    customer validation

--- routes/customer_route.js
+++ routes/customer_route.js
@@ -1,10 +1,11 @@
 const express = require ("express");
 const { findAll,save, findById, deleteById, update } = require("../controller/customer_controller");
+const CustomerValidation = require("../validation/customer_validation");
 const Router= express.Router();
 
 
 Router.get("/", findAll);
-Router.post("/",save);
+Router.post("/",CustomerValidation,save);
 Router.get("/:id",findById);
 Router.delete("/:id",deleteById);
 Router.put("/:id",update);
---
+++ validation/customer_validation.js
@@ -0,0 +1,21 @@
+const joi =require("joi");
+
+const customerSchema= joi.object({
+    full_name:joi.string().required(),
+    email:joi.string().required().email(),
+    contact_no:joi.string().required(),
+    address:joi.string().required()
+
+})
+
+function CustomerValidation(req,res,next){
+    const{full_name,email,contact_no,address}=req.body;
+    const{error}=customerSchema.validate({full_name,email,contact_no,address})
+    if(error){
+          return  res.json("Data Validaition Fail")
+
+    }
+    next()
+}
+
+module.exports=CustomerValidation
